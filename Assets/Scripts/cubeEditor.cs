876a7e403026bfaa32fd290862fdbbe5f6fbf2b6
nananoizy <morihuelacazorla@gmail.com>
2020-02-07 16:40:07 +0100

    textMesh with position of the cube

--- Assets/Scripts/cubeEditor.cs
+++ Assets/Scripts/cubeEditor.cs
@@ -3,17 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [ExecuteInEditMode]
-public class editorSnap : MonoBehaviour
+public class cubeEditor : MonoBehaviour
 {
 
     [Range(1f,20f)] [SerializeField] float gridSize = 10f;
+
+    TextMesh textMesh;
+
+    void Start(){
+        
+        textMesh = GetComponentInChildren<TextMesh>();
+
+    }
+
     void Update()
     {
         Vector3 snapPos;
         snapPos.x = Mathf.RoundToInt(transform.position.x/10f) * gridSize;
         snapPos.z = Mathf.RoundToInt(transform.position.z/10f) * gridSize;
 
+        textMesh.text = snapPos.x + "," + snapPos.z;
+
         transform.position = new Vector3(snapPos.x, 0f, snapPos.z);
     }
-
 }
