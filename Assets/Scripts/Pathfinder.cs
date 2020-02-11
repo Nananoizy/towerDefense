07492b4e5b724366d88edb2ac1bc25053d719dda
nananoizy <morihuelacazorla@gmail.com>
2020-02-11 15:49:32 +0100

    Pathfinding basis

---
+++ Assets/Scripts/Pathfinder.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pathfinder : MonoBehaviour
+{
+
+    Dictionary<Vector2Int, Waypoint> grid = new Dictionary<Vector2Int, Waypoint>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        LoadBlocks();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void LoadBlocks(){
+
+        var waypoints = FindObjectsOfType<Waypoint>();
+
+        foreach (Waypoint waypoint in waypoints){
+
+            bool isOverlapping = grid.ContainsKey(waypoint.GetGridPos());
+
+            if (!isOverlapping){
+                grid.Add(waypoint.GetGridPos(), waypoint);
+            }
+            else{
+                Debug.LogWarning("Overlapping block: " + waypoint.GetGridPos());
+            }
+            
+        }
+
+    }
+}
--- Assets/Scripts/Waypoint.cs
+++ Assets/Scripts/Waypoint.cs
@@ -23,7 +23,7 @@ public class Waypoint : MonoBehaviour
         return gridSize;
     }
 
-    public Vector2 GetGridPos(){
+    public Vector2Int GetGridPos(){
         return new Vector2Int(
             Mathf.RoundToInt(transform.position.x/10f) * gridSize,
             Mathf.RoundToInt(transform.position.z/10f) * gridSize
