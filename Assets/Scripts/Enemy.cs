570e15b642ad42d3bea43848c5511166ea684f55
nananoizy <morihuelacazorla@gmail.com>
2020-02-12 17:27:05 +0100

    Tower shooting particles

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] GameObject enemyDeathFX;
+    [SerializeField] int hits = 3;
+
+
+
+    void Start()
+    {
+    }
+
+    void OnParticleCollision(GameObject other){
+        
+        hits--;
+
+        if (hits <= 0){
+            KillEnemy();
+            Destroy(other);
+        }
+    }
+
+    void KillEnemy(){
+
+        Instantiate(enemyDeathFX, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+
+    }
+
+}
