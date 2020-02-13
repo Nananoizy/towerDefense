4eb3468c39fc1a70d7af162b49702e442596220a
nananoizy <morihuelacazorla@gmail.com>
2020-02-13 13:54:00 +0100

    Add health to base

---
+++ Assets/Scripts/FriendlyBase.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FriendlyBase : MonoBehaviour
+{
+
+    [SerializeField] int health = 10;
+    [SerializeField] int healthDecrease = 1;
+
+
+    void OnTriggerEnter(Collider collider){
+
+        health = health - healthDecrease;
+
+    }
+}
